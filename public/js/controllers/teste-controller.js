62bebac6489fead79434679cf3544a5a73640f17
Rodrigo Marzullo <rodrigo.marzullo@gmail.com>
2018-05-21 21:19:32 -0300

    Hello Word

---
+++ public/js/controllers/teste-controller.js
@@ -0,0 +1,8 @@
+angular
+  .module("angularServiceNow")
+  .controller("FotosController", function($scope) {
+    $scope.foto = {
+      nome: "Leao",
+      url: "https://www.ahnegao.com.br/wp-content/uploads/2018/05/img-2-2.jpg"
+    };
+  });
